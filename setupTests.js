1d8cde3e557d83c7cb57376b5efffd0e580b0499
Prudence <prudence@blueanchor.io>
2019-03-27 10:47:00 +0100

    deleted files

--- setupTests.js
+++
@@ -1,5 +0,0 @@
-const Enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16.3');
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
